acb7fca29f4ac21c4622f2eefe0554c78526ddfd
Dmitry K <akinc@yandex.ru>
2022-04-25 22:09:24 +0300

    First 9 chapters completed

---
+++ tutor.rb
@@ -0,0 +1,52 @@
+#Twinkle-twinkle little star
+loop do
+  use_synth :fm
+  play rrand_i(50,80), amp: rrand(0.1, 0.3)
+  sleep 0.25
+end
+
+3.times do
+  sample :perc_snap
+  sleep 1
+end
+use_synth :dark_ambience
+use_bpm 120
+
+sample :perc_snap, rate: -1
+sleep 1
+
+5.times do
+  play :c4
+  sleep 1
+  play :c4, amp: 0.5
+  sleep 1
+  play :g4
+  sleep 1
+  play :g4
+  sleep 1
+  play :a4
+  sleep 1
+  play :a4
+  sleep 1
+  play :g4
+  sleep 2
+  play :f4
+  sleep 1
+  play :f4
+  sleep 1
+  play :e4
+  sleep 1
+  play :e4
+  sleep 1
+  play :d4
+  sleep 1
+  play :d4
+  sleep 1
+  play :c4
+  sleep 1
+end
+#synth :tb303, note: :e2 - 0, release: 12, cutoff: 90
+#synth :tb303, note: :e2 - 4, release: 12, cutoff: 90
+#synth :tb303, note: :e2 - 4, release: 82, cutoff: 90
+#synth :tb303, note: :e4 - 4, release: 12, cutoff: 90
+#synth :tb303, note: :e2 - 4, release: 12, cutoff: 90
